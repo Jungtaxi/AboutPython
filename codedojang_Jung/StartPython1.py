3baf09fd8f8ab82710053933010e8a7442572c56
Jung <beagentleman7@gmail.com>
2022-03-23 03:25:56 +0900

    2장 데이터 타입의 문자열 파트 끝

---
+++ codedojang_Jung/StartPython1.py
@@ -0,0 +1,16 @@
+import keyword
+print(keyword.kwlist)
+print("파이썬의 키워드 들입니다.\n")
+
+# 출력 print()
+print("Hello Coding Python")
+# 빈 줄을 출력합니다.
+print()
+# 출력하고 싶은 것들을 나열합니다.
+print("hi",2,"my")
+print()
+
+print("------ 확인 전용 선 ----------")
+print()
+print()
+print("------ 확인 전용 선 ----------")
