93d8339630ea089b84373994bfd0de1c5cddee86
Jung <beagentleman7@gmail.com>
2022-03-30 02:16:57 +0900

    코딩도장 튜플전까지함

---
+++ dojang python/9.4practice.py
@@ -0,0 +1,18 @@
+print("""\
+'Python' is a "programming language"
+that lets you work quickly
+and
+integrate systems more effectively.\
+""")
+
+
+a = range(-10,10,3)
+print(type(a))
+print(a)
+
+b = list(range(-10,10,3))
+print(b)
+
+c = 1, 2, 3
+print(type(c))
+print(c)
