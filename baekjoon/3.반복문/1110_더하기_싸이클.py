66f87dbd63d009b572f73e9f18647a1ad31b0e61
JungTaekKim <beagentleman7@gmail.com>
2022-04-16 14:11:19 +0900

    makeFolderBaekJoon

--- baekjoon/3.반복문/1110_더하기_싸이클.py
+++ baekjoon/3.반복문/1110_더하기_싸이클.py
@@ -1,3 +1,17 @@
+# N = int(input())
+# tens = N//10
+# ones = N%10
+# iter = 0
+# while True :
+#     iter += 1
+#     num = ones 
+#     ones = (ones+tens)%10
+#     tens = num
+#     if N == 10*tens + ones :
+#         break
+# print(iter)
+
+
 N = int(input())
 iter = 0
 num = -1
