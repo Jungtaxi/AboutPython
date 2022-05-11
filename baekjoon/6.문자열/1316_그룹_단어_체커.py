9d7cd698e911a1b8fc70cf94b448823e39d92d9c
JungTaekKim <beagentleman7@gmail.com>
2022-05-11 13:39:01 +0900

    1316

--- baekjoon/6.문자열/1316_그룹_단어_체커.py
+++ baekjoon/6.문자열/1316_그룹_단어_체커.py
@@ -1,4 +1,16 @@
 T = int(input())
-for i in range(T):
-    word = input()
-    
+count = 0
+for _ in range(T):
+    s = input()    
+    if len(s)==1:
+        count += 1
+        continue
+    elif len(s) == 2:
+        count += 1
+        continue
+    for idx, item in enumerate(s[:-1]):
+        if item != s[idx+1]:
+            if s.count(item,idx+1) > 0:
+                break
+    else: count += 1
+print(count)
